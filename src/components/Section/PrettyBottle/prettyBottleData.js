18a4a6ffac8bca86379f15b9a685d4aa42fd0614
Bohdan <boghdan.shimko@gmail.com>
2024-02-17 00:45:44 +0200

    fix: src in swiper

--- src/components/Section/PrettyBottle/prettyBottleData.js
+++ src/components/Section/PrettyBottle/prettyBottleData.js
@@ -1,41 +1,45 @@
+import bottleOne from '../../../assets/images/pretty-bottle/bottle-one.png'
+import bottleThree from '../../../assets/images/pretty-bottle/bottle-three.png'
+import bottleTwo from '../../../assets/images/pretty-bottle/bottle-two.png'
+
 const prettyBottleData = [
   {
-    src: 'src/assets/images/pretty-bottle/bottle-one.png',
+    src: bottleOne,
     title: 'The Daedreamer Set',
     price: '$49.00',
     alt: 'The Daedreamer Set',
     id: 1,
   },
   {
-    src: 'src/assets/images/pretty-bottle/bottle-two.png',
+    src: bottleTwo,
     title: 'Shampoo',
     price: '$24.00 to $64.00',
     alt: 'Shampoo',
     id: 2,
   },
   {
-    src: 'src/assets/images/pretty-bottle/bottle-three.png',
+    src: bottleThree,
     title: 'Conditioner',
     price: '$24.00 to $64.00',
     alt: 'Conditioner',
     id: 3,
   },
   {
-    src: 'src/assets/images/pretty-bottle/bottle-one.png',
+    src: bottleOne,
     title: 'The Daedreamer Set',
     price: '$49.00',
     alt: 'The Daedreamer Set',
     id: 4,
   },
   {
-    src: 'src/assets/images/pretty-bottle/bottle-two.png',
+    src: bottleTwo,
     title: 'Shampoo',
     price: '$24.00 to $64.00',
     alt: 'Shampoo',
     id: 5,
   },
   {
-    src: 'src/assets/images/pretty-bottle/bottle-three.png',
+    src: bottleThree,
     title: 'Conditioner',
     price: '$24.00 to $64.00',
     alt: 'Conditioner',
